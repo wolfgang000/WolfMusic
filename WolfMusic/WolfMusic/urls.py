41633f33aa6d464f11f8b36ab54ec7991156065c
wolfgang000 <wolfgangsalazarmora@hotmail.com>
2016-09-04 22:07:06 -0400

    Add static and media folder

--- WolfMusic/WolfMusic/urls.py
+++ WolfMusic/WolfMusic/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 	url(r'^admin/', admin.site.urls),
 	url(r'^api/', include('wolf_services.urls')),
 	url(r'^wolfy/', include('main_site.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
