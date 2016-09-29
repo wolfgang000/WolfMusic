a94f6a963d7e9bc3d2ec89cd668e3064fd42dc24
wolfgang000 <wolfgangsalazarmora@hotmail.com>
2016-09-29 01:04:07 -0400

    Improve autoplay Add accidentally concurrency problems

--- WolfMusic/static/main/script.js
+++ WolfMusic/static/main/script.js
@@ -16,19 +16,58 @@ var currentContext = {
 	tracks:[],
 	iterator:{},
 	it:{},
+	setTracksFromAlbum : function (albums){
+		this.tracks=[];
+		albums.forEach(
+			function (item, index) {
+				item.tracks.forEach(
+					function (item, index) {
+						console.log(this.tracks)
+						this.tracks.push(item.url);
+					},this);
+			},this);
+	},
 	play :  function (index){
 			if(index == null) {
 				index = 0;
 			}
+			console.log("play")
+			console.log(index)
 			this.iterator = makeIterator(this.tracks,index);
+			console.log(this.tracks)
+			console.log(this.iterator)
+			console.log("pre")
 			this.playNext();
+			console.log("prost")
 		},
-		
+	
+	getResourse : 
+		function(url) {
+			return Promise.resolve(
+				$.ajax({
+					url: url,
+					tryCount : 0,
+					retryLimit : 3
+				})
+			);
+		},
 	
 	playNext : function (){
+		console.log("en play")
 			this.it = this.iterator.next();
+			console.log(this.it)
 			if(!this.it.done){
-				setPlayerSource(this.it.value)
+				console.log("pre get")
+				this.getResourse(this.it.value).then(
+					function(response) {
+						console.log("success")
+						setPlayerSource(response)
+					},
+					function(error) {
+						 console.error("Failed!", error);
+					}
+				);
+				console.log("post get")
 			}
 		} 
 };
@@ -86,29 +125,35 @@ var setUploadFileForm  = function() {
 
 var loadList = function() {
 	$.ajax({
-		url: url.track,
+		url: url.album,
 		success: function(data) {
 			console.log(data);
-			currentContext.tracks = data;
+			currentContext.setTracksFromAlbum(data);
 			list = document.getElementById("list");
 			list.innerHTML = "";
+			globalIndex=0;
 			data.forEach(
 				function (item, index) {
-					var divId = "divTrack"+index;
-					var div = document.createElement("div");
-					div.setAttribute("id",divId);
-					
-					var lable = document.createElement("lable");
-					lable.innerHTML = item.title;
-					var btn = document.createElement("button");
-					btn.innerHTML = "Select";
-					btn.onclick = function() {currentContext.play(index)};
-										
-					div.appendChild(lable)
-					div.appendChild(btn);
-					div.appendChild(document.createElement("br"));
-
-					list.appendChild(div);
+					item.tracks.forEach(
+						function (item, index) {
+							var divId = "divTrack"+index;
+							var div = document.createElement("div");
+							div.setAttribute("id",divId);
+							
+							var lable = document.createElement("lable");
+							lable.innerHTML = item.title;
+							var btn = document.createElement("button");
+							btn.innerHTML = "Select";
+							console.log(globalIndex)
+							var auxIndex = globalIndex;
+							btn.onclick = function() {currentContext.play(auxIndex)};
+												
+							div.appendChild(lable)
+							div.appendChild(btn);
+							div.appendChild(document.createElement("br"));
+							list.appendChild(div);
+							globalIndex++;
+						});
 				}
 			)
 		}
