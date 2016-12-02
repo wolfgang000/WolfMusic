27869abcc9d3fd171f88f0c53445f9dd685070f9
wolfgang000 <wolfgangsalazarmora@hotmail.com>
2016-12-02 01:30:30 -0400

    Delete useless importations

--- WolfMusic/tests.py
+++ WolfMusic/tests.py
@@ -1,16 +1,6 @@
 from django.test import TestCase
-from django.test import Client
 from django.urls import reverse
-from django.core.files import File
-from rest_framework.test import APIClient
 from rest_framework import status
-import codecs
-import os
-
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
 
 class SwaggerAPITests(TestCase):
 	def test_get(self):
