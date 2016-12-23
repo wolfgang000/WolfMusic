8935568388a096ad600ac3ac40f62340118b05e9
wolfgang000 <wolfgangsalazarmora@hotmail.com>
2016-12-23 12:20:30 -0400

    Make web page branch

--- WolfMusic/main_site/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MainSiteConfig(AppConfig):
-    name = 'main_site'
--- WolfMusic/wolf_services/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class WolfServicesConfig(AppConfig):
-    name = 'wolf_services'
