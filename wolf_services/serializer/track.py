4c3e2a50c12cb4fee0d04ebd6033ffe489b8a621
wolfgang000 <wolfgangsalazarmora@hotmail.com>
2016-12-26 17:59:46 -0400

    Add url to track serializer

--- wolf_services/serializer/track.py
+++ wolf_services/serializer/track.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
+from django.core.urlresolvers import reverse
 
 class TrackSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=256)
-    track_number = serializers.IntegerField()
+	id = serializers.IntegerField()
+	title = serializers.CharField(max_length=256)
+	track_number = serializers.IntegerField()
+	url = serializers.SerializerMethodField()
+	
+	def get_url(self, obj):
+		return reverse('track.detail', args=[str(obj.id)])
--- wolf_services/test/unit/test_serializers.py
+++ wolf_services/test/unit/test_serializers.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.conf import settings
+from django.core.urlresolvers import reverse
+
 import os
 from ...serializer.track import TrackSerializer
 from ...entities.track import Track
@@ -9,4 +11,7 @@ class TrackSerializerTests(TestCase):
 	def test_instantiation(self):
 		track = Track(title='ranchera', track_number=1, id=1)
 		serializer = TrackSerializer(track)
-		self.assertDictEqual({'title': 'ranchera', 'track_number': 1, 'id': 1},serializer.data)
+		self.assertDictEqual(
+								{'title': 'ranchera', 'track_number': 1, 'id': 1, 'url' : reverse('track.detail', args=[str(1)])},
+								serializer.data
+							)
